319d207845e4a78532b5cfd8f29e76ddcfb9c978
ravenNNN <baltazarparra@outlook.com>
2016-03-19 21:50:46 -0300

    Adicionando diferença nos tratamentos referente ao sexo.

--- imc-calc.js
+++ imc-calc.js
@@ -16,19 +16,19 @@ var altura = validaPonto.charAt(0) + '.' + validaPonto.charAt(1) + validaPonto.c
 var indice = peso / (altura * altura);
 var imc = indice.toFixed(2);
 
-	//if (document.getElementById('homem').checked) {
-	//	rate_value = document.getElementById('homem').value;
-	//	var sexo = document.createTextNode('HOMEM');
-	//} else {
-	//	var sexo = document.createTextNode('MULHER');
-	//}
+	if (document.getElementById('homem').checked) {
+		rate_value = document.getElementById('homem').value;
+		var sexo = 'E ai mano, ';
+	} else {
+		var sexo = 'Ei flor, ';
+	}
 
 	var para = document.createElement("h1");
 
-	if (imc < 25) {
-		var node = document.createTextNode(imc + ' ' + 'Seu imc está abaixo do normal, você precisa começar a comer direito.');
-	} else if (imc > 25 ) {
-		var node = document.createTextNode(imc + ' ' + 'Seu imc está Alto! vamos começar a fechar a boquinha?');
+	if (imc < 24) {
+		var node = document.createTextNode(imc + ' ' + sexo + 'seu imc está abaixo do normal, você precisa começar a comer direito.');
+	} else if (imc > 26 ) {
+		var node = document.createTextNode(imc + ' ' + sexo + 'seu imc está Alto! vamos começar a fechar a boquinha?');
 	}
 
 	para.appendChild(node);
